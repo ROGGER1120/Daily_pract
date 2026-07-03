81f0520092598184a08d391b33d5845c8457632d
Harikrishnan S <harikrishnan3522@gmail.com>
2026-07-03 23:27:45 +0530

    1st Problem solved

---
+++ Hexaware Qyestion/ReverseString.py
@@ -0,0 +1,17 @@
+##Get a input as a string and return the string in reverse
+
+#writting the function to reverse the string
+def Reverse_String(string):#function to reverse the string and passing the parameter as string
+    str1 = string[::-1]#using slicing method to reverse the string
+    #returning the str1 as reversed string and user get the output as reversed string
+    return str1
+
+
+string = input("Enter a string: ")
+#calling the function and printing the reversed string
+print("Reversed_string :" , Reverse_String(string))
+#printing the reversed string using written function Reverse_String()
+
+
+## Input : Hello
+## Output : olleH
---
+++ Hexaware Qyestion/txt.md
@@ -0,0 +1,15 @@
+Hexaware Programming question
+Coding Questions
+
+Easy Level
+
+Reverse a string.
+Check whether a number is prime.
+Find the largest element in an array.
+Find the second-largest element.
+Remove duplicates from an array.
+Check if a string is a palindrome.
+Count vowels in a string.
+Fibonacci series.
+Factorial using recursion.
+Swap two numbers without a third variable.
